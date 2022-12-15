9c800a48be793d2e74728b852d25254c943b6ffc
hoangtc125 <tranconghoang2k1@gmail.com>
2022-12-15 09:16:10 +0700

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 const multer = require('multer');
-const {responseError, callRes} = require('./response/error');
+const {responseError, callRes} = require('./api/response/error');
 
 const app = express()
 
@@ -21,16 +21,16 @@ mongoose.connect(url,
     );
 
 // use Routes
-app.use('/it4788/auth', require('./routes/auth'));
-app.use('/it4788/friend', require('./routes/friend'));
-app.use('/it4788/post', require('./routes/posts'));
-app.use('/it4788/search', require('./routes/search'));
-app.use('/it4788/comment', require('./routes/comments'));
-app.use('/it4788/like', require('./routes/likes'));
-app.use('/it4788/friend', require('./routes/friend'));
-app.use('/it4788/setting', require('./routes/settings'));
-app.use('/it4788/user', require('./routes/user'));
-app.use('/it4788/chat', require('./routes/chat'));
+app.use('/it4788/auth', require('./api/routes/auth'));
+app.use('/it4788/friend', require('./api/routes/friend'));
+app.use('/it4788/post', require('./api/routes/posts'));
+app.use('/it4788/search', require('./api/routes/search'));
+app.use('/it4788/comment', require('./api/routes/comments'));
+app.use('/it4788/like', require('./api/routes/likes'));
+app.use('/it4788/friend', require('./api/routes/friend'));
+app.use('/it4788/setting', require('./api/routes/settings'));
+app.use('/it4788/user', require('./api/routes/user'));
+app.use('/it4788/chat', require('./api/routes/chat'));
 app.use(function (err, req, res, next) {
     if(err instanceof multer.MulterError) {
         if(err.code === 'LIMIT_UNEXPECTED_FILE') {
